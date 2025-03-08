f5ae29c7c3c2126d640dbc306bca413ac2410c25
TaxiDriver202 <topias.silfverhuth@gmail.com>
2025-03-08 19:29:46 +0200

    version 1.1

--- sacpi.c
+++ sacpi.c
@@ -17,186 +17,227 @@
  */
 
 
- #include <unistd.h>
- #include <fcntl.h>
- #include <getopt.h>
- #include <stdlib.h>
- #include <stdio.h>
- #include <string.h>
- #include <dirent.h>
- #include "config.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
 
- /* function declarations  */
- static void bat(const char *bdir);
- static void read_ac();
- static int batscan(const struct dirent *bat);
- static void read_thermal(const char *tdir);
+#define VERSION "1.1"
+
+/* function declarations  */
+
+static void bat(const char *bdir);
+static void read_ac(const char *acdir);
+static int batscan(const struct dirent *bat);
+static void read_thermal(const char *tdir);
+
+/* Directories for battery & ac, thermal info respectively. */
+static const char *adir = "/sys/class/power_supply";
+static const char *tdir = "/sys/class/thermal";
+
+/* Scans for all batteries in bdir(for scandir)  */
+int
+batscan(const struct dirent *bat)
+{
+  if(strstr(bat->d_name, "BAT"))
+    return 1;
+  else if(strstr(bat->d_name, "bat"))
+    return 1;
+  else
+    return 0;
+}
+
+/** Displays battery information **/
+void
+bat(const char *bdir)
+{
+  int i;
+  struct dirent **batteries;
+  int c;
+  c = scandir(bdir, &batteries, batscan, alphasort);  
+
  
- 
- static const char *adir = "/sys/class/power_supply";
- static const char *tdir = "/sys/class/thermal";
- 
- /* Scans for all batteries in bdir(for scandir)  */
- int
- batscan(const struct dirent *bat)
- {
-   if(strstr(bat->d_name, "BAT"))
+  for(i=0; i<c; i++){
+    /* Read Capacity */
+    char *capn;
+    int capf;
+
+    asprintf(&capn, "%s/%s/capacity", bdir, batteries[i]->d_name);
+    if(capn)
+      capf = open(capn , O_RDONLY);
+    free(capn);
+
+    ssize_t capsize = 0;
+    char capbuf[10];
+    if(capf > 0)
+      capsize = read(capf, &capbuf, 9);
+    close(capf);
+    
+    char capacity[capsize];
+    snprintf(capacity, capsize, "%s", capbuf);
+    
+    /* Read status */
+    char stn[265];
+    snprintf(stn, 265, "%s/%s/status", bdir, batteries[i]->d_name);
+    /* open returns non-zero if successful, -1 if not */
+    int st = open(stn, O_RDONLY);
+    
+    char stbuf[20];
+    ssize_t stsize = 0;
+    if(st>0)
+      stsize = read(st, &stbuf, 20);
+    close(st);
+    
+    char state[stsize];
+    snprintf(state, stsize, "%s", stbuf);
+
+    printf("%s: %s%%, %s\n", batteries[i]->d_name, capsize ? capacity : "0", stsize ? state : "");
+  }
+}
+
+/**  AC adapter info  **/
+void
+read_ac(const char *acdir){
+  char *acn;
+  int acf;
+  asprintf(&acn, "%s/%s/online", acdir, "AC");
+  
+  if(acn)
+    acf = open(acn , O_RDONLY);
+  free(acn);
+
+  char buf[2];
+  if(acf)
+    read(acf, &buf, 1);
+  close(acf);
+  short online = atoi(buf);
+  
+  printf("Adapter: %s\n", online ? "on-line" : "off-line");
+}
+
+/* scandir filter function  */
+int
+thermscan(const struct dirent *dir)
+{
+  if(strstr(dir->d_name, "thermal_zone"))
      return 1;
-   else if(strstr(bat->d_name, "bat"))
-     return 1;
-   else
+  else
      return 0;
- }
- 
- /* Displays battery information */
- void
- bat(const char *bdir)
- {
-   int i;
-   struct dirent **batteries;
-   int c;
-   c = scandir(bdir, &batteries, batscan, alphasort);  
- 
+}
+
+/**  Thermals  **/
+void
+read_thermal(const char *tdir)
+{
+  struct dirent **thermals;
+  int d;
+  int i;
+  ssize_t ts = 0;
+  char thermbuf[5];
+  int therm;
+  int tf;
   
-   for(i=0; i<c; i++){
-     /** Read Capacity **/
-     char capn[266];
-     snprintf(capn, 266, "%s/%s/capacity", bdir, batteries[i]->d_name);
-     int capf = open(capn , O_RDONLY);
-     
-     ssize_t capsize = 0;
-     char capbuf[10];
-     if(capf > 0)
-       capsize = read(capf, &capbuf, 9);
-     close(capf);
-     
-     char capacity[capsize];
-     snprintf(capacity, capsize, "%s", capbuf);
-     
-     /** Read status **/
-     char stn[265];
-     snprintf(stn, 265, "%s/%s/status", bdir, batteries[i]->d_name);
-     /* open returns non-zero if successful, -1 if not */
-     int st = open(stn, O_RDONLY);
-     
-     char stbuf[20];
-     ssize_t stsize = 0;
-     if(st>0)
-       stsize = read(st, &stbuf, 20);
-     close(st);
-     
-     char state[stsize];
-     snprintf(state, stsize, "%s", stbuf);
- 
-     printf("%s: %s%%, %s\n", batteries[i]->d_name, capsize ? capacity : "0", stsize ? state : "");
- }
- }
- 
- /*  AC adapter info  */
- void
- read_ac(){
-   int ac = open("/sys/class/power_supply/AC/online", O_RDONLY);
-   char buf[2];
-   if(ac) read(ac, &buf, 1);
-   close(ac);
-   short online = atoi(buf);
-   
-   printf("Adapter: %s\n", online ? "on-line" : "off-line");
- }
- 
- /* scandir filter function  */
- int
- thermscan(const struct dirent *dir)
- {
-   if(strstr(dir->d_name, "thermal_zone"))
-      return 1;
-   else
-      return 0;
- }
- 
- /*  Thermals  */
- void
- read_thermal(const char *tdir)
- {
-   struct dirent **thermals;
-   int d;
-   int i;
-   ssize_t ts = 0;
-   char thermbuf[5];
-   int therm;
-   
-   d = scandir(tdir, &thermals, thermscan, alphasort);
- 
-   for(i=0; i<d; i++){
-     char zn[265];
-     snprintf(zn, 265, "%s/%s/temp", tdir, thermals[i]->d_name);
-     int tf = open(zn, O_RDONLY);
- 
-     if(tf>0)
-       ts = read(tf, &thermbuf, 5);
-     close(tf);
-     therm = atoi(thermbuf);
-     therm /= 1000;
- 
-     if(!ts)
-       fprintf(stderr, "Thermal %d: Not Found\n", i);
-     printf("Thermal %d: %d C\n", i, therm);
-   }
- }
- 
- void
- help(){
-   puts("Usage: sacpi [option]\noptions are: -b for battery info, -a for AC adapter info");
- }
- 
- static void
- version(){
-  printf("sacpi %s\na simple tool to display battery, AC, and thermal info.\n\
-Copyright (C) 2025 Topias Silfverhuth\n", VERSION);
-  puts("License GPLv2+: GNU GPL version 2 or later <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>\n\
-This program is free software: you are free to change and redistribute it.\n\
+  d = scandir(tdir, &thermals, thermscan, alphasort);
+
+  for(i=0; i<d; i++){
+    char *zn;
+    asprintf(&zn, "%s/%s/temp", tdir, thermals[i]->d_name);
+    if(zn)
+      tf = open(zn, O_RDONLY);
+    free(zn);	
+
+    if(tf>0)
+      ts = read(tf, &thermbuf, 5);
+    close(tf);
+    therm = atoi(thermbuf);
+    therm /= 1000;
+
+    if(!ts)
+      fprintf(stderr, "Thermal %d: Not Found\n", i);
+    printf("Thermal %d: %d C\n", i, therm);
+  }
+}
+
+static void
+help(){
+  puts("Usage: sacpi [option]\noptions are: \n\
+-b, --battery      for battery info \n\
+-a, --ac           for AC adapter info\n\
+-t, --thermal      for thermal info\n\
+-A, --all          prints all the options\n\
+-h, --help         prints this help\n\
+-v, --version	   displays the version and license information");
+}
+
+static void
+version(){
+  printf("sacpi %s\n", VERSION);
+  puts("a simple tool to display battery, AC, and thermal info.\n\
+Copyright (C) 2025 Topias Silfverhuth\n\
+License GPLv2+: GNU GPL version 2 or later <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>\n\
+This is free software: you are free to change and redistribute it.\n\
 There is NO WARRANTY, to the extent permitted by law.\n");
- }
- 
- int
- main(int argc, char *argv[]){
- 
-   int c;
-   unsigned short bflag = 0;
-   unsigned short acflag = 0;
-   unsigned short tflag = 0;
-   
-   while((c=getopt(argc, argv, "abhtv")) != -1)
-     switch(c){
-     case 'b':
-       bflag=1;
-       break;
-     case 'a':
-       acflag=1;
-       break;
-     case 't':
-       tflag=1;
-       break;
-     case 'v':
-       version();
-       break;
-     case 'h':
-       help();
-       break;
-     case '?':
-       help();
-       break;
-     }
+}
+
+
+int
+main(int argc, char *argv[]){
+
+  int c;
+  unsigned short bflag = 0;
+  unsigned short acflag = 0;
+  unsigned short tflag = 0;
+
+  static struct option longopts[] =
+    {
+      {"version", no_argument, 0, 'v'},
+      {"help", no_argument, 0, 'h'},
+      {"battery", no_argument, 0, 'b'},
+      {"thermal", no_argument, 0, 't'},
+      {"ac", no_argument, 0, 'a'},
+      {"all", no_argument, 0, 'A'}
+    };
+  int optindex = 0;
   
-   if(acflag)
-     read_ac();
-   if(bflag)
-     bat(adir);
-   if(tflag)
-     read_thermal(tdir);
-   if(argc==1)
-     bat(adir);
-   
-   return EXIT_SUCCESS;
- }
+  
+  while((c=getopt_long(argc, argv, "Aabhtv", longopts, &optindex)) != -1)
+    switch(c){
+    case 'b':
+      bflag=1;
+      break;
+    case 'a':
+      acflag=1;
+      break;
+    case 't':
+      tflag=1;
+      break;
+    case 'A':
+      bflag=1;
+      acflag=1;
+      tflag=1;
+      break;
+    case 'h':
+      help();
+      break;
+    case 'v':
+      version();
+      break;
+    case '?':
+      help();
+      break;
+    }
  
+  if(acflag)
+    read_ac(adir);
+  if(bflag)
+    bat(adir);
+  if(tflag)
+    read_thermal(tdir);
+  if(argc==1)
+    bat(adir);
+  
+  return EXIT_SUCCESS;
+}
